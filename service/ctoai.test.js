fd92110c5c199a0a89b5eec0d4314acc68c11ea1
Brian St. Amand <brian@cto.ai>
2020-09-15 19:11:28 -0700

    e2e tests

--- service/ctoai.test.js
+++ service/ctoai.test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
-describe('Test Upsidedown', function () {
-    describe('upside()', function () {
-          it('should be present when called', function () {
-                  assert.equal(1,1);
-                });
-        });
+describe('Test Upsidedown', function() {
+  describe('upside()', function() {
+    it('should be present when called', function() {
+      assert.equal(1, 1);
+    });
+  });
 });
