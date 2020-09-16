b77aec24f2158b26d3934922f88465898bc3d884
Brian St. Amand <brian@cto.ai>
2020-09-15 19:11:41 -0700

    more e2e tests

---
+++ service/ctoai.e2etest.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+describe('API Accessible', function() {
+  describe('fetch()', function() {
+    it('API responds with 200', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
