4d101914048f797c744608c412f5c1d14e0cb5a2
Brian St. Amand <brian@cto.ai>
2020-09-18 00:30:09 -0700

    fix linting

--- service/ctoai.test.js
+++ service/ctoai.test.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 
 describe('Upside Suite', function() {
-
   describe('upside()', function() {
     it('should be present when called', function() {
       assert.equal(1, 1);
@@ -19,9 +18,7 @@ describe('Upside Suite', function() {
       assert.equal(1, 1);
     });
   });
-
   describe('Middleware transformations working as intended', function() {
-
     it('correct output', function() {
       assert.equal(1, 1);
     });
@@ -31,9 +28,7 @@ describe('Upside Suite', function() {
     it('transformation is valid', function() {
       assert.equal(1, 1);
     });
-
   });
-
   describe('in-order sequence', function() {
     it('series completes', function() {
       assert.equal(1, 1);
@@ -42,5 +37,4 @@ describe('Upside Suite', function() {
       assert.equal(1, 1);
     });
   });
-
 });
