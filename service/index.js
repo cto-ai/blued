52563e335c3f8ce33791686cc932cc593ef4030c
Steve Hiehn <stevehiehn@hackcapital.com>
2020-09-28 15:07:55 -0700

    fix lint issues in the service

--- service/index.js
+++ service/index.js
@@ -1,4 +1,4 @@
-const { sdk } = require('@cto.ai/sdk')
+const {sdk} = require('@cto.ai/sdk');
 const site = require('node-static');
 const http = require('http');
 
@@ -8,34 +8,33 @@ http.createServer(function(req, res) {
   file.serve(req, res);
 
   const config = require('./config.json');
-  const pkg = require('./package.json')
+  const pkg = require('./package.json');
 
-  if(config.CTOAI_API_URL === 'https://api.cto.ai') {
+  if (config.CTOAI_API_URL === 'https://api.cto.ai') {
     sdk.track('Deployment', {
       pipeline_id: 'cto-ai/blued',
       stage: 'Deployment',
       status: 'Succeeded',
-      stage_ref: pkg.version
-    })
+      stage_ref: pkg.version,
+    });
   } else {
     sdk.track('Deployment', {
       pipeline_id: 'cto-ai/blued',
       stage: 'Deployment',
       status: 'Failure',
-      stage_ref: pkg.version
-    })
+      stage_ref: pkg.version,
+    });
   }
 
-  console.log('server started!')
-  console.log(process.env)
+  console.log('server started!');
+  console.log(process.env);
 }).listen(8080);
 
-process.on('uncaughtException', err => {
+process.on('uncaughtException', (err) => {
   sdk.track('Deployment', {
     pipeline_id: 'cto-ai/blued',
     stage: 'Deployment',
     status: 'Failure',
-    stage_ref: pkg.version
-  })
-
-})
+    stage_ref: pkg.version,
+  });
+});
